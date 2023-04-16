343d8bdaf8601b6cc4874426025e8a69dc2049fc
Aleksandr <meynspain@gmail.com>
2023-04-16 13:15:56 +0900

    #comment 2 Добавлено: Первичная инициализация приложения, Переворачивание экрана перед созданием MaterialApp, Убрана верхняя строка состояния.

--- lib/game_core/main_loop.dart
+++ lib/game_core/main_loop.dart
@@ -1,1 +1,37 @@
+//Нижнее подчеркивание это модификатор private
 bool _running = true;
+
+///Запуск тика и обновления картинки в игре
+void startLoop() {
+  final double _fps = 50;
+  final double _second = 1000;
+  final double _updateTime = _second / _fps; //Каждые updateTime милисекунд нужно обновлять игру
+  double _updates = 0;
+
+  //Стандартный таймер, который будет считать сколько времени прошло
+  Stopwatch _loopWatch = Stopwatch();
+  _loopWatch.start();
+
+  Stopwatch _timerWatch = Stopwatch();
+  _timerWatch.start();
+
+  while (_running) {
+    //Каждый updateTime обновляем картинку
+    if (_loopWatch.elapsedMilliseconds > _updateTime) {
+      _updates++;
+      _loopWatch.reset();
+    }
+
+    if (_timerWatch.elapsedMilliseconds > _second){
+      print('${DateTime.now()} FPS: $_updates');
+      _updates = 0;
+      _timerWatch.reset();
+    
+    }
+  }
+}
+
+///Остановка тика и обновления картинки в игре
+void stopLoop() {
+  _running = false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_demo_game/game_core/main_loop.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  //Сначала повернется экран, а затем уже создаться MaterialApp
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+
+        //Убирает верхнюю строку со временем и всем остальным
+        SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
+
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Game'),
+      ),
+    ));
+  });
 }
